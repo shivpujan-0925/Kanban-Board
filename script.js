523ef01b84ccb49f301992d5fb9fef766aeff572
shivpujan <kumarshivpujan9087@gmail.com>
2026-02-22 15:40:43 +0530

    push to github

---
+++ script.js
@@ -0,0 +1,53 @@
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
+
+for( const card of cards){
+   card.addEventListener("dragstart", dragStart );
+   card.addEventListener("dragend", dragEnd);
+
+}
+
+for(const list of lists){
+       list.addEventListener("dragover", dragOver)
+       list.addEventListener("dragenter", dragEnter)
+       list.addEventListener("dragleave", dragLeave)
+       list.addEventListener("drop", dragDrop)
+
+}
+
+function dragStart(e){
+       //this allow when the drop location to know which element is being moved when you  release it 
+       
+       e.dataTransfer.setData("text/plain", this.id)
+       console.log("Dragging id: " , this.id);
+       
+
+}
+
+function dragEnd(){
+       console.log("Drag Ended");
+       
+}
+
+function dragOver(e){
+       e.preventDefault()
+}
+
+function dragEnter(e) {
+       e.preventDefault();
+       this.classList.add("over")
+}
+
+function dragLeave(e) {
+       this.classList.remove("over");
+}
+
+function dragDrop(e){
+  const id = e.dataTransfer.getData("text/plain");
+  const card = document.getElementById(id);
+
+  this.appendChild(card);
+  this.classList.remove("over");
+
+
+}
